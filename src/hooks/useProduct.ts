ae2cc474be19448e7f1073770b6d13868dbe5dd8
Mihretab <48807537+Almesty21@users.noreply.github.com>
2025-12-12 10:52:12 +0300

    Update useProduct.ts

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ProductPayload } from "../types";
-import { getProducts } from "../services/productService";
+import { getProducts } from "../services/product";
 
 interface UseProductReturn {
   data: ProductPayload[];
