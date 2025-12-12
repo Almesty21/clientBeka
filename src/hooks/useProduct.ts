7f8ce823a7321c62921e20ae45bc861a3ef6ea77
Mihretab <48807537+Almesty21@users.noreply.github.com>
2025-12-12 10:58:56 +0300

    Update useProduct.ts

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,18 +1,29 @@
+// src/hooks/useProduct.ts
 import { useState, useEffect } from "react";
 import { ProductPayload } from "../types";
 import { getProducts } from "../services/products";
 
-interface UseProductReturn {
+export interface UseProductReturn {
   data: ProductPayload[];
   loading: boolean;
   error: string | null;
   fetchData: () => Promise<void>;
+
+  openActivate: boolean;
+  openBan: boolean;
+  handleOpenActivate: () => void;
+  handleCloseActivate: () => void;
+  handleOpenBan: () => void;
+  handleCloseBan: () => void;
 }
 
 export default function useProduct(): UseProductReturn {
   const [data, setData] = useState<ProductPayload[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+
+  const [openActivate, setOpenActivate] = useState(false);
+  const [openBan, setOpenBan] = useState(false);
 
   const fetchData = async () => {
     setLoading(true);
@@ -37,5 +48,12 @@ export default function useProduct(): UseProductReturn {
     loading,
     error,
     fetchData,
+
+    openActivate,
+    openBan,
+    handleOpenActivate: () => setOpenActivate(true),
+    handleCloseActivate: () => setOpenActivate(false),
+    handleOpenBan: () => setOpenBan(true),
+    handleCloseBan: () => setOpenBan(false),
   };
 }
