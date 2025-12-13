055f5a087bdfd05315a160f904881d03ac50ef56
Mihretab <48807537+Almesty21@users.noreply.github.com>
2025-12-13 12:47:37 +0300

    Update index.tsx

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -14,21 +14,26 @@ export default function Register() {
     <div className="w-full flex h-screen justify-center items-center px-4 py-4 bg-login bg-center bg-cover bg-no-repeat">
       <div className="w-full sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0 m-4">
         <div
-          className="w-full bg-white rounded-lg overflow-clip"
+          className="w-full bg-white rounded-lg overflow-hidden"
           style={{ boxShadow: "0 4px 25px 0 rgba(0,0,0,.1)" }}
         >
           <div className="flex w-full items-center">
             <div className="hidden lg:block lg:w-1/2">
               <img src="" alt="logo" className="w-full h-full" />
             </div>
+
             <div className="w-full lg:w-1/2 p-8">
-              <div className="w-full flex flex-col gap-y-4">
+              <div className="flex flex-col gap-y-2 mb-4">
                 <h4 className="font-semibold text-lg">Register</h4>
                 <p className="text-base font-normal">
-                  Welcome back, please Register to your account.
+                  Welcome, please register your account.
                 </p>
               </div>
-              <form onSubmit={handleSubmit(onSubmit)} className="w-full flex flex-col gap-y-4 mt-5">
+
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="w-full flex flex-col gap-y-4"
+              >
                 {/* Username */}
                 <Controller
                   name="username"
@@ -56,23 +61,24 @@ export default function Register() {
                   )}
                 />
 
-                <div className="w-full">
+                <div className="flex justify-between items-center mt-4">
                   <Button
                     loading={loading}
                     htmlType="submit"
                     type="primary"
-                    className="py-2 px-8 rounded-md float-left h-auto font-medium w-fit shadow-none"
+                    className="py-2 px-8 h-auto font-medium"
                   >
                     Register
                   </Button>
-                  <p className="footer-text float-right pt-6">
-                    Already have an account? 
+
+                  <div className="flex items-center gap-1">
+                    <span>Already have an account?</span>
                     <Link to={RouteName.LOGIN}>
-                      <Button type="link" className="top-2.5 w-fit float-right text-teal-400">
+                      <Button type="link" className="text-teal-400 p-0">
                         Login
                       </Button>
                     </Link>
-                  </p>
+                  </div>
                 </div>
               </form>
             </div>
